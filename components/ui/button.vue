2e3c7084fce9d14ad5748f8d1812d367cf981384
Quentin guerrier <75quentinguerrier@gmail.com>
2022-11-07 15:51:56 +0100

    add button component

---
+++ components/ui/button.vue
@@ -0,0 +1,21 @@
+<template>
+  <button :type="type" class="flex items-center justify-center" :class="getStyle()"><slot /></button>
+</template>
+<script lang="ts" setup>
+// Props & emits
+const props = defineProps<{
+  type: 'submit' | 'button'
+  color: 'primary' | 'secondary' | 'terciary'
+}>()
+
+// Methods
+const getStyle = () => {
+  if (props.color === 'primary') {
+    return 'inline-flex items-center rounded-md border border-transparent bg-blue-600 px-4 py-3 text-sm font-medium text-white shadow-sm hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2'
+  } else if (props.color === 'secondary') {
+    return 'inline-flex items-center rounded-md border border-gray-300 bg-white px-4 py-3 text-sm font-medium text-gray-700 shadow-sm hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2'
+  } else if (props.color === 'terciary') {
+    return 'flex items-center text-sm font-medium text-gray-500 hover:text-gray-700'
+  }
+}
+</script>
