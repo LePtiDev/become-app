0277d02071f2b0815523a2ee02625fa71c70dbd6
Quentin guerrier <75quentinguerrier@gmail.com>
2022-11-07 15:52:36 +0100

    add components spacer

---
+++ components/ui/spacer.vue
@@ -0,0 +1,8 @@
+<template>
+  <div class="flex w-full my-[24px] items-center">
+    <span class="w-full border-b border-gray-400"></span>
+    <p class="text-gray-400 text-sm whitespace-nowrap mx-[8px]"><slot /></p>
+    <span class="w-full border-b border-gray-400"></span>
+  </div>
+</template>
+<script lang="ts" setup></script>
